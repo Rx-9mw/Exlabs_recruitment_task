265f079b13fb4075ffca3b81e90f40e273e84b5c
Rx8mw <smonek33@gmail.com>
2025-01-11 14:42:25 +0100

    added connection to database

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@users.hunzc.mongodb.net/?retryWrites=true&w=majority&appName=Users`);
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Connected to Database.');
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
